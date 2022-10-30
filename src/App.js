d69800b620b6f1e97387c916efed57248575e5dc
zama ngubane <zngubane@student.wethinkcode.co.za>
2022-10-30 20:48:26 +0200

    added icon for linked in, name of my profile and my title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
+import Profile from './PortfolioContainer/Home/Profile';
 
 function App() {
   return (
     <div className="App">
-      
+      <Profile/>
     </div>
   );
 }
---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Typical from 'react-typical'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+      <div className='profile-parent'>
+            <div className='colz'>
+                <a href='https://www.linkedin.com/in/zamakhosi-ngubane-b464281b7/'>
+                    <i className='fa fa-linkedin-square'></i>
+                </a>
+            </div>
+            <div className='profile-details-name'>
+                <span className='primary-text'>
+                    {" "}
+                    Hello, I'm <span className='highlighted-text'>Zamakhosi Ngubane</span>
+                </span>
+            </div>
+            <div className='profie-details-role'>
+                <span className='primary-text'>
+                    {" "}
+                    <h1>
+                        {" "}
+                        <Typical
+                        loop={Infinity}
+                        steps={[
+                            "Software Engineering Graduate",
+                            1000,
+                            "Full Stack Developer",
+                            1000,
+                        ]}
+                        />
+                    </h1>
+                </span>
+            </div>
+         <div className='profile-details'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
