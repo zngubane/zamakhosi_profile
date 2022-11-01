968793aa21a4b2450776c0c49b64fb4cab4aeff1
zama ngubane <zngubane@student.wethinkcode.co.za>
2022-11-01 16:15:33 +0200

    added button on form

--- src/PortfolioContainer/Home/Contact/Contact.js
+++ src/PortfolioContainer/Home/Contact/Contact.js
@@ -29,7 +29,7 @@ export default function Contact() {
                 <input type="text" name='name' placeholder='Your Full Name' required />
                 <input type= "email" name='email' placeholder='Your Email' required/>
                 <textarea name='message' rows='7' placeholder='Your Message' required/>
-                
+                <button type='submit' className='btn btn-primary'>Send Message</button>
             </form>
         </div>
     </div>
